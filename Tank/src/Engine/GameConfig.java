c2dc7c8fedae811c72bbfcafe4fec221eb1d3aa1
Mr-sww <1743768308@qq.com>
2024-12-16 17:36:38 +0800

    地图变大

--- Tank/src/Engine/GameConfig.java
+++ Tank/src/Engine/GameConfig.java
@@ -5,8 +5,8 @@ package Engine;
  */
 public class GameConfig {
     // 窗口大小
-    public static final int PANEL_WIDTH = (60*13);
-    public static final int PANEL_HEIGHT = (60*10);
+    public static final int PANEL_WIDTH = (60*21);
+    public static final int PANEL_HEIGHT = (60*21);
 
     // 坦克默认速度
     public static final int TANK_SPEED = 6;
